fb3a08c2e1205642ed96eb62e80a8a604c05b75d
Daniel Höller <hoeller@cs.uni-saarland.de>
2021-08-27 16:42:13 +0200

    made cplex run

---
+++ src/search/heuristics/liftedRP.cpp
@@ -0,0 +1,9 @@
+//
+// Created by dh on 27.08.21.
+//
+
+#include "liftedRP.h"
+
+int liftedRP::compute_heuristic(const DBState &s, const Task &task) {
+    return 0;
+}
---
+++ src/search/heuristics/liftedRP.h
@@ -0,0 +1,17 @@
+//
+// Created by dh on 27.08.21.
+//
+
+#ifndef SEARCH_LIFTEDRP_H
+#define SEARCH_LIFTEDRP_H
+
+#include <ilcplex/ilocplex.h>
+#include "heuristic.h"
+
+class liftedRP : public Heuristic{
+public:
+    int compute_heuristic(const DBState & s, const Task& task) final;
+};
+
+
+#endif //SEARCH_LIFTEDRP_H
