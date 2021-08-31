ca00bac5349d77e8681d4f944f8a3b5e6854a9fc
Daniel Höller <hoeller@cs.uni-saarland.de>
2021-08-31 14:25:04 +0200

    implemented type check of parameters

--- src/search/heuristics/liftedRP.cpp
+++ src/search/heuristics/liftedRP.cpp
@@ -1,37 +1,230 @@
 //
-// Created by dh on 27.08.21.
+// Created by Daniel Höller on 27.08.21.
 //
 
 #include "liftedRP.h"
+#include <unordered_set>
+
+using namespace std;
+
+liftedRP::liftedRP(const Task task) {
+    numActions = task.actions.size();
+    numObjs = task.objects.size();
+    for (auto a: task.actions) {
+        maxArity = max(maxArity, (int) a.get_parameters().size());
+    }
+
+    cout << "- re-inferring type hierarchy..." << endl;
+    int numTypes = task.type_names.size();
+    types = new unordered_set<int>[numTypes];
+    for (int obj = 0; obj < task.objects.size(); obj++) {
+        auto oTypes = task.objects[obj].getTypes();
+        for (int i = 0; i < oTypes.size(); i++) {
+            int j = oTypes[i];
+            types[j].insert(obj);
+        }
+    }
+    unordered_set<int> parents[numTypes];
+    for (int child = 0; child < numTypes; child++) {
+        for (int parent = 0; parent < numTypes; parent++) {
+            if (child == parent) continue;
+            if (types[child].size() < types[parent].size()) {
+                bool isSubset = true;
+                for (int elem: types[child]) {
+                    if (types[parent].find(elem) == types[parent].end()) {
+                        isSubset = false;
+                        break;
+                    }
+                }
+                if (isSubset) {
+                    parents[child].insert(parent);
+                }
+            }
+        }
+    }
+
+    // make sparse
+    for (int i = 0; i < numTypes; i++) {
+        for (int j = 0; j < numTypes; j++) {
+            if (i == j) continue;
+            for (int k = 0; k < numTypes; k++) {
+                if ((i == k) || (j == k)) continue;
+                if ((parents[i].find(j) != parents[i].end())
+                    && (parents[j].find(k) != parents[j].end())
+                    && (parents[i].find(k) != parents[i].end())) {
+                    parents[i].erase(parents[i].find(k));
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < numTypes; i++) {
+        cout << task.type_names[i] << " -> {";
+        bool first = true;
+        for (auto p : parents[i]) {
+            if (first) {
+                first = false;
+            } else {
+                cout << ", ";
+            }
+            cout << task.type_names[p];
+        }
+        cout << "}" << endl;
+    }
+
+    children = new unordered_set<int>[numTypes];
+    ps = new unordered_set<int>[numTypes];
+    toptasks = new unordered_set<int>;
+    for (int i = 0; i < numTypes; i++) {
+        if (parents[i].size() == 0) {
+           toptasks->insert(i);
+        } else {
+            for (int p : parents[i]) {
+                children[p].insert(i);
+                ps[i].insert(p);
+            }
+        }
+    }
+
+    upperTindex = new int[numTypes];
+    lowerTindex = new int[numTypes];
+    objToIndex = new int[numObjs];
+    indexToObj = new int[numObjs];
+
+    int r = 0;
+    for (int t : *toptasks) {
+        r = sortObjs(r, t);
+    }
+
+    for (int i = 0; i < task.objects.size(); i++) {
+        cout << task.objects[i].getName() << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < numTypes; i++) {
+        cout << task.type_names[i] << ":\t" << lowerTindex[i] << "-" << upperTindex[i] << " = {";
+        for(int j = lowerTindex[i]; j <= upperTindex[i]; j++) {
+            if (j > lowerTindex[i]) {
+                cout << ", ";
+            }
+            int k = indexToObj[j];
+            cout << task.objects[k].getName();
+        }
+        cout << "}" << endl;
+    }
+
+    cout << "- num actions " << numActions << endl;
+    cout << "- num objects " << numObjs << endl;
+    cout << "- max arity " << maxArity << endl;
+}
 
 int liftedRP::compute_heuristic(const DBState &s, const Task &task) {
+    IloEnv env;
+    IloNumVarArray v(env);
+    IloModel model(env);
 
-    cout << "I am here!" << endl;
+    // create variables representing lifted relaxed plan
+    // a1 p1_1 p1_2 ... p1_k a2 p2_1 p2_2 ... p2_k ... an pn_1 pn_2 ... pn_k
+    for (int i = 0; i < planLength; i++) {
+        v.add(IloNumVar(env, 0, numActions, IloNumVar::Int));
+        string vName = "a" + to_string(i);
+        v[v.getSize() - 1].setName(vName.c_str());
+        for (int j = 0; j < maxArity; j++) {
+            v.add(IloNumVar(env, 0, numObjs, IloNumVar::Int));
+            string vName2 = "p" + to_string(i);
+            vName2 += "_" + to_string(j);
+            v[v.getSize() - 1].setName(vName2.c_str());
+            model.add(v[actionID(i)] * largeC >= v[paramID(i, j)]); // if action schema is 0, the parameter also need to be 0
+        }
+    }
 
-    IloEnv lenv;
-    IloNumVarArray v(lenv);
-    IloModel model(lenv);
+    // create main optimization function
+    //for (int i = 0; i < planLength; i+=maxArity) {
+    //}
 
-    v.add(IloNumVar(lenv, 0, 1, IloNumVar::Bool));
-    IloNumExpr mainExp(lenv);
+    // enforce typing of action parameters
+
+    for (int i = 0; i < planLength; i++) {
+        int iSchema = actionID(i);
+        for (int j = 1; j <= numActions; j++) { // actions with base 1 (!)
+            cout << task.actions[j - 1].get_name() << endl;
+            IloConstraint schemaEq = (v[iSchema] == j);
+            auto params = task.actions[j - 1].get_parameters();
+            for (int l = 0; l < params.size(); l++) {
+                cout << "- " << task.type_names[params[l].type] << ": ";
+                int lower = lowerTindex[params[l].type];
+                int upper = upperTindex[params[l].type];
+                for (int m = lower; m <= upper; m++) {
+                    cout << task.objects[indexToObj[m]].getName() << " ";
+                }
+                cout << endl;
+                int iP = paramID(i, l);
+                IloIfThen lowerVarRange(env, schemaEq, (v[iP] - 1) > (lower - 1));
+                IloIfThen upperVarRange(env, schemaEq, (v[iP] - 1) < (upper + 1));
+                model.add(lowerVarRange);
+                model.add(upperVarRange);
+            }
+        }
+    }
+    //model.add(v[0] == 3);
+
+    IloNumExpr mainExp(env);
     mainExp = mainExp + (v[0]);
-    model.add(IloMinimize(lenv, mainExp));
+    model.add(IloMinimize(env, mainExp));
     IloCplex cplex(model);
+    cplex.exportModel("/home/dh/Schreibtisch/LiftedDelRel/test.lp");
     cplex.setParam(IloCplex::Param::Threads, 1);
 
     // no output
-    // cplex.setOut(lenv.getNullStream());
-    // cplex.setWarning(lenv.getNullStream());
+    // cplex.setOut(env.getNullStream());
+    // cplex.setWarning(env.getNullStream());
 
     int res = -1;
     if (cplex.solve()) {
         res = round(cplex.getObjValue()); // todo: use ceil for LP
-        double d = cplex.getValue(v[0]);
+        //cout << "a" << "=" << round(cplex.getValue(v[0])) << endl;
+        //cout << "b" << "=" << round(cplex.getValue(v[1])) << endl;
+
+        for (int i = 0; i < planLength; i++) {
+            int x = round(cplex.getValue(v[actionID(i)])) - 1;
+            if (x >= 0) {
+                cout << "a" << i << "=" << task.actions[x].get_name();
+            }
+            for (int j = 0; j < maxArity; j++) {
+                int x = round(cplex.getValue(v[paramID(i, j)])) - 1;
+                if(x >= 0) {
+                    cout << " p" << j << "=" << task.objects[indexToObj[x]].getName();
+                }
+            }
+            cout << endl;
+        }
     }
+    cout << "res: " << res << endl;
     exit(0);
     return 0;
 }
 
-liftedRP::liftedRP(const Task task) {
+int liftedRP::sortObjs(int index, int type) {
+    lowerTindex[type] = index;
+    for (int st : children[type]) {
+        index = sortObjs(index, st);
+    }
+    for(int o : types[type]) {
+        if (done.find(o) == done.end()) {
+            objToIndex[o] = index;
+            indexToObj[index] = o;
+            index++;
+            done.insert(o);
+        }
+    }
+    upperTindex[type] = index - 1;
+    return index;
+}
 
+int liftedRP::actionID(int i) {
+    return (maxArity + 1) * i;
+}
+
+int liftedRP::paramID(int i, int j) {
+    return (maxArity + 1) * i + (j + 1);
 }
--- src/search/heuristics/liftedRP.h
+++ src/search/heuristics/liftedRP.h
@@ -9,10 +9,36 @@
 #include "heuristic.h"
 
 class liftedRP : public Heuristic{
+private:
+    const int largeC = 100000;
+    int planLength = 2;
+    int maxArity = -1;
+
+    int numObjs = -1;
+    int numActions = -1;
+
+    int* lowerTindex;
+    int* upperTindex;
+    int* objToIndex;
+    int* indexToObj;
+
+    unordered_set<int>* types;
+
+    unordered_set<int>* children;
+    unordered_set<int>* ps;
+    unordered_set<int>* toptasks;
+    unordered_set<int> done;
+
+    int sortObjs(int index, int type);
 public:
+
     liftedRP(const Task task);
 
     int compute_heuristic(const DBState & s, const Task& task) final;
+
+    int actionID(int i);
+
+    int paramID(int i, int j);
 };
 
 
