1b27953591e5ddae4923c963d407144ddc5f2135
Galvus Damor <galvusdamor@gmail.com>
2022-10-25 16:29:07 +0200

    Build script

--- build.py
+++ build.py
@@ -17,6 +17,8 @@ def parse_arguments():
                                      description='Build Power Lifted planner.')
     parser.add_argument('-d', '--debug',
                         action="store_true", help="Build in debug mode.")
+    parser.add_argument('-s', '--sat', action="store", help="Build with SAT support. Provide the path to the directory of the SAT solver library.", default="none")
+    parser.add_argument('-i', '--ipasir', action="store_true", help="Use IPASIR SAT Solver. By default we link against kissat")
     return parser.parse_args()
 
 def get_build_dir(debug):
@@ -30,7 +32,7 @@ def create_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def build(debug_flag):
+def build(debug_flag, sat, ipasir):
     BUILD_DIR = get_build_dir(debug_flag)
     BUILD_SEARCH_DIR = os.path.join(BUILD_DIR, 'search')
     if debug_flag:
@@ -40,11 +42,18 @@ def build(debug_flag):
     create_dir(BUILD_DIR)
     create_dir(BUILD_SEARCH_DIR)
     copy_tree(TRANSLATOR_DIR, BUILD_DIR + '/translator')
-    subprocess.check_call(['cmake', SEARCH_DIR, '-DCMAKE_BUILD_TYPE='+BUILD_TYPE],
+    cmakeArguments = ['cmake', SEARCH_DIR, '-DCMAKE_BUILD_TYPE='+BUILD_TYPE]
+    if sat != 'none':
+        cmakeArguments.append('-DSAT=ON')
+        if ipasir:
+            cmakeArguments.append('-DKISSAT=OFF')
+        cmakeArguments.append('-DSAT_DIR=' + sat)
+
+    subprocess.check_call(cmakeArguments,
                           cwd=BUILD_SEARCH_DIR)
     subprocess.check_call(['make', '-j5'], cwd=BUILD_SEARCH_DIR)
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    build(args.debug)
+    build(args.debug, args.sat, args.ipasir)
